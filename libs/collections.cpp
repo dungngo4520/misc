7beddd39ba16d52df806c185586c4fdb4d6691a3
Dung Ngo <dungngo4520@gmail.com>
2025-07-24 23:38:03 +0700

    add libs template

---
+++ libs/collections.cpp
@@ -0,0 +1,6 @@
+void test(int a, int b) {
+    // This is a test function that does nothing.
+    // It is used to ensure that the file compiles correctly.
+    int result = a + b;
+    (void)result; // Prevent unused variable warning
+}
